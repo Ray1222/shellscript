cf67e8dc50367c0cb1a5756164b0cb9435a42868
Ray1222 <ljr1222@gmail.com>
2017-08-23 15:07:52 +0800

    v2

---
+++ aide-report.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+#keli Aide report by ljr1222
+DATE=`date +%Y-%m-%d`
+REPORT="Aide-Report-ktli.apps.sinica.edu.tw-"$DATE
+echo "Aide-Report-ktli.apps.sinica.edu.tw `date`" > /tmp/$REPORT
+/usr/sbin/aide -c aide-ktli.conf >> /tmp/$REPORT
+echo "****************DONE******************" >> /tmp/$REPORT
+
+#ADD=$(cat /tmp/$REPORT|grep Added|awk -F " " '{print $1":"$3}')
+#REMV=$(cat /tmp/$REPORT|grep Removed|awk -F " " '{print $1":"$3}')
+#CHG=$(cat /tmp/$REPORT|grep Changed|awk -F " " '{print $1":"$3}')
+
+STATE=`cat /tmp/$REPORT|grep -e okay |awk '{print $8}'`
+#ALL="$ADD ;$REMV ;$CHG"
+
+if [ $STATE = "okay!" ]
+then
+
+ALL=OK!
+
+else
+
+ALL=`cat /tmp/$REPORT|sed -n '/Total/,/Changed files/p'|sed ':a;N;$!ba;s/\n/ /g'`
+fi
+echo $ALL
+cat << _EOT_ | \
+        /usr/sbin/smtp-cli --server=gate.sinica.edu.tw \
+                   --from=sysinfo@gate.sinica.edu.tw \
+                   --to=ljr1222@gate.sinica.edu.tw \
+                   --subject="$REPORT $ALL" \
+                   --body-plain=/tmp/$REPORT
+
+_EOT_
+
+
